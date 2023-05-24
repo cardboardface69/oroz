82c0971beb947a231b3e25d8b6a66d682679070d
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-05-24 12:54:54 +0530

    Update anilist.py

--- anibot/plugins/anilist.py
+++ anibot/plugins/anilist.py
@@ -319,7 +319,7 @@ async def mana_cmd(client: Client, message: Message):
           pass
 @anibot.on_message(filters.chat(-1001944303479) & filters.regex("gogoanime"))
 async def mana_cmd(client: Client, message: Message):
-      try:
+      try:
          ser = message.from_user.username
          zex = await message.reply_text(f"@{ser}, Unfortunately forwarding any links of our website will be deleted as to not violate any **Telegram Terms of Service**.")
          await message.delete()
