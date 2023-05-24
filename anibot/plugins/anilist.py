cb65372db5ed7caf9686d83d8daa323fef852e69
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-05-24 09:59:10 +0530

    Update anilist.py

--- anibot/plugins/anilist.py
+++ anibot/plugins/anilist.py
@@ -128,7 +128,12 @@ async def mana_cmd(client: Client, message: Message):
       except:
 
           pass
-@anibot.on_message(filters.chat(-1001944303479) & filters.regex("9anime" or "9ani" or "4anime" or "nhentai" or "animix" or "an!m!x" or "animixplay" or "Animixplay" or "Nhentai" or "g o g o a n i m e" or "twist.moe" or "animepahe" or "Animepahe" or "Animebee" or "animebee" or "kissanime"))
+siz = '''"9anime", "9ani", "4anime", "nhentai", "animix", "an!m!x", "animixplay", "Animixplay", "Nhentai", "g o g o a n i m e", "twist.moe", "animepahe", "Animepahe", "Animebee", "animebee", "kissanime"'''
+
+
+
+
+@anibot.on_message(filters.chat(-1001944303479) & filters.regex(siz))
 
 async def mana_cmd(client: Client, message: Message):
 
