feb7c378fbb9af48e61ddb904603e76a3bb6bb8c
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-05-25 19:36:54 +0530

    Update anilist.py

--- anibot/plugins/anilist.py
+++ anibot/plugins/anilist.py
@@ -15,7 +15,7 @@ from pyrogram import filters, Client
 from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, InputMediaPhoto, Message
 import urllib.request
 from pyrogram.errors import UserNotParticipant
-from .. import ANILIST_CLIENT, ANILIST_REDIRECT_URL,HELP_DICT ANILIST_SECRET, OWNER, TRIGGERS as trg, BOT_NAME, anibot
+from .. import ANILIST_CLIENT, ANILIST_REDIRECT_URL,HELP_DICT, ANILIST_SECRET, OWNER, TRIGGERS as trg, BOT_NAME, anibot
 from ..utils.data_parser import (
     get_all_genres, search_filler, parse_filler, get_all_tags, get_scheduled, get_top_animes, get_user_activity, get_user_favourites, toggle_favourites,
     get_anime, get_airing, get_anilist, get_character, get_wo, get_wols, get_additional_info, get_manga, browse_,
