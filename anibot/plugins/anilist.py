18c094fcbfed4d9fc5ba4f7f448024b6f621942f
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-05-28 11:53:24 +0530

    Update anilist.py

--- anibot/plugins/anilist.py
+++ anibot/plugins/anilist.py
@@ -1542,7 +1542,7 @@ zoro_id = -1001662381049
 async def mana_cmd(client: Client, message: Message):
 
          ser = str(message.from_user.first_name)
-         pattern = "^[GINKO]+$"
+         pattern = ".*[GgIiNnKkOo]+.*"
          hu = int(message.from_user.id)
          ginlink = f"https://telegram.me/share/url?url=/ban%20{hu}"
          repl_markup=InlineKeyboardMarkup([[InlineKeyboardButton(
