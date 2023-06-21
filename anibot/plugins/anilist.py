537c5a8d55cf1411be717a86cfb71a903ae00092
cardboardface69 <108939863+cardboardface69@users.noreply.github.com>
2023-06-21 20:03:13 +0530

    Update anilist.py

--- anibot/plugins/anilist.py
+++ anibot/plugins/anilist.py
@@ -159,7 +159,7 @@ async def filler_btn(client: anibot, cq: CallbackQuery, cdata: dict):
     fillerx = await cq.edit_message_text(msg)
     await asyncio.sleep(180)
     return await fillerx.delete()           
-@anibot.on_message(filters.chat(-1001944303479) &  & ~filters.user(ADMIN) & filters.regex("zoro.to"))
+@anibot.on_message(filters.chat(-1001944303479) & ~filters.user(ADMIN) & filters.regex("zoro.to"))
 async def mana_cmd(client: Client, message: Message):
 
          ser = message.from_user.username
